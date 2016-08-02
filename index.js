f83597f53a39dda6f97143454953209c266298be
Andreia Fidalgo <andreiafidalgo@Andreias-MacBook-Air.local>
2016-08-02 23:31:58 +0200

    proper variable assignment

--- index.js
+++ index.js
@@ -1,102 +1,114 @@
-var yourdata_transportmode = document.getElementById('yourdata_transportmode');
-var yourdata_distance = document.getElementById('yourdata_distance');
+var yourdata_transportmode = document.getElementById('yourdata_transportmode')
+var yourdata_distance = document.getElementById('yourdata_distance')
+
+var yourcosts_total = document.getElementById('yourcosts_total');
+var yourcosts_direct = document.getElementById('yourcosts_direct');
+var yourcosts_time = document.getElementById('yourcosts_time');
+var yourcosts_external = document.getElementById('yourcosts_external');
+var yourcosts_annual = document.getElementById('yourcosts_annual');
+
+var yourtaxdeduction_deduction = document.getElementById('yourtaxdeduction_deduction');
+var yourtaxdeduction_daily = document.getElementById('yourtaxdeduction_daily');
+var yourtaxdeduction_annual = document.getElementById('yourtaxdeduction_annual');
+
+
 
 onChangeTransportMode = function(event) {
-    calculateCosts(event);
+    calculateCosts(event)
 }
 
 document.getElementById('yourdata_distance').addEventListener('input', function(event) {
-    calculateCosts(event);
-});
-
-
+    calculateCosts(event)
+})
 
 calculateCosts = function(event) {
 
-
+  console.log(yourdata_distance.value);
 
     switch (true) {
 
         // Caso: carros diesel
-        case document.getElementById('yourdata_transportmode').value === 'car_diesel':
-            document.getElementById('yourcosts_total').value = document.getElementById('yourdata_distance').value * 10
-            document.getElementById('yourcosts_direct').value = 0;
-            document.getElementById('yourcosts_time').value = 0;
-            document.getElementById('yourcosts_external').value = 0;
-            document.getElementById('yourcosts_annual').value = 0;
+        case yourdata_transportmode.value === 'car_diesel':
 
-            document.getElementById('yourtaxdeduction_deduction').value = 0;
-            document.getElementById('yourtaxdeduction_daily').value = 0;
-            document.getElementById('yourtaxdeduction_annual').value = 0;
+            yourcosts_total.value = document.getElementById('yourdata_distance').value * 10
+            yourcosts_direct.value = 0
+            yourcosts_time.value = 0
+            yourcosts_external.value = 0
+            yourcosts_annual.value = 0
 
-            break;
+            yourtaxdeduction_deduction.value = 0
+            yourtaxdeduction_daily.value = 0
+            yourtaxdeduction_annual.value = 0
 
-            // Caso: carros gasoline
-        case document.getElementById('yourdata_transportmode').value === 'car_gasoline':
-            document.getElementById('yourcosts_total').value = document.getElementById('yourdata_distance').value * 10
-            document.getElementById('yourcosts_direct').value = 0;
-            document.getElementById('yourcosts_time').value = 0;
-            document.getElementById('yourcosts_external').value = 0;
-            document.getElementById('yourcosts_annual').value = 0;
+            break
 
-            document.getElementById('yourtaxdeduction_deduction').value = 0;
-            document.getElementById('yourtaxdeduction_daily').value = 0;
-            document.getElementById('yourtaxdeduction_annual').value = 0;
+        // Caso: carros gasoline
+        case  yourdata_transportmode.value === 'car_gasoline':
+            yourcosts_total.value = document.getElementById('yourdata_distance').value * 10
+            yourcosts_direct.value = 0
+            yourcosts_time.value = 0
+            yourcosts_external.value = 0
+            yourcosts_annual.value = 0
 
-            break;
+            yourtaxdeduction_deduction.value = 0
+            yourtaxdeduction_daily.value = 0
+            yourtaxdeduction_annual.value = 0
 
-
-        case document.getElementById('yourdata_transportmode').value === 'car_electric':
-            document.getElementById('yourcosts_total').value = document.getElementById('yourdata_distance').value * 10
-            document.getElementById('yourcosts_direct').value = 0;
-            document.getElementById('yourcosts_time').value = 0;
-            document.getElementById('yourcosts_external').value = 0;
-            document.getElementById('yourcosts_annual').value = 0;
-
-            document.getElementById('yourtaxdeduction_deduction').value = 0;
-            document.getElementById('yourtaxdeduction_daily').value = 0;
-            document.getElementById('yourtaxdeduction_annual').value = 0;
-            break;
+            break
 
 
-        case document.getElementById('yourdata_transportmode').value === 'bus':
-            document.getElementById('yourcosts_total').value = document.getElementById('yourdata_distance').value * 10
-            document.getElementById('yourcosts_direct').value = 0;
-            document.getElementById('yourcosts_time').value = 0;
-            document.getElementById('yourcosts_external').value = 0;
-            document.getElementById('yourcosts_annual').value = 0;
+        case  yourdata_transportmode.value === 'car_electric':
+            yourcosts_total.value = document.getElementById('yourdata_distance').value * 10
+            yourcosts_direct.value = 0
+            yourcosts_time.value = 0
+            yourcosts_external.value = 0
+            yourcosts_annual.value = 0
 
-            document.getElementById('yourtaxdeduction_deduction').value = 0;
-            document.getElementById('yourtaxdeduction_daily').value = 0;
-            document.getElementById('yourtaxdeduction_annual').value = 0;
+            yourtaxdeduction_deduction.value = 0
+            yourtaxdeduction_daily.value = 0
+            yourtaxdeduction_annual.value = 0
+            break
 
-            break;
 
-        case document.getElementById('yourdata_transportmode').value === 'train':
-            document.getElementById('yourcosts_total').value = document.getElementById('yourdata_distance').value * 10
-            document.getElementById('yourcosts_direct').value = 0;
-            document.getElementById('yourcosts_time').value = 0;
-            document.getElementById('yourcosts_external').value = 0;
-            document.getElementById('yourcosts_annual').value = 0;
+        case  yourdata_transportmode.value === 'bus':
+            yourcosts_total.value = document.getElementById('yourdata_distance').value * 10
+            yourcosts_direct.value = 0
+            yourcosts_time.value = 0
+            yourcosts_external.value = 0
+            yourcosts_annual.value = 0
 
-            document.getElementById('yourtaxdeduction_deduction').value = 0;
-            document.getElementById('yourtaxdeduction_daily').value = 0;
-            document.getElementById('yourtaxdeduction_annual').value = 0;
+            yourtaxdeduction_deduction.value = 0
+            yourtaxdeduction_daily.value = 0
+            yourtaxdeduction_annual.value = 0
 
-            break;
+            break
 
-        case document.getElementById('yourdata_transportmode').value === 'bicycle':
-            document.getElementById('yourcosts_total').value = document.getElementById('yourdata_distance').value * 10
-            document.getElementById('yourcosts_direct').value = 0;
-            document.getElementById('yourcosts_time').value = 0;
-            document.getElementById('yourcosts_external').value = 0;
-            document.getElementById('yourcosts_annual').value = 0;
+        case  yourdata_transportmode.value === 'train':
+            yourcosts_total.value = document.getElementById('yourdata_distance').value * 10
+            yourcosts_direct.value = 100
 
-            document.getElementById('yourtaxdeduction_deduction').value = 0;
-            document.getElementById('yourtaxdeduction_daily').value = 0;
-            document.getElementById('yourtaxdeduction_annual').value = 0;
+            yourcosts_time.value = 0
+            yourcosts_external.value = 0
+            yourcosts_annual.value = 0
 
-            break;
+            yourtaxdeduction_deduction.value = 0
+            yourtaxdeduction_daily.value = 0
+            yourtaxdeduction_annual.value = 0
+
+            break
+
+        case  yourdata_transportmode.value === 'bicycle':
+            yourcosts_total.value = document.getElementById('yourdata_distance').value * 10
+            yourcosts_direct.value = 0
+            yourcosts_time.value = 0
+            yourcosts_external.value = 0
+            yourcosts_annual.value = 0
+
+            yourtaxdeduction_deduction.value = 0
+            yourtaxdeduction_daily.value = 0
+            yourtaxdeduction_annual.value = 0
+
+            break
 
 
     }
